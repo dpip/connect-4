5ff87e851618d9829af18a9cffa8f273885e40d9
dpip <dpip.nc@gmail.com>
2023-08-22 16:37:32 -0400

    connects to mongo and creates user schema

--- connect-4-backend/server.js
+++ connect-4-backend/server.js
@@ -1,7 +1,51 @@
+require('dotenv').config();
+
 // server.js
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
+
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbCluster = process.env.DB_CLUSTER;
+
 const port = process.env.PORT || 3000;
+const MONGODB_URI = `mongodb+srv://${dbUser}:${dbPassword}@${dbCluster}.oiqyui1.mongodb.net/?retryWrites=true&w=majority`;
+
+// Eestablish connection to MongoDB
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', (error) =>
+  console.error('MongoDB connection error:', error)
+);
+db.once('open', () => console.log('Connected to MongoDB Atlas'));
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+});
+
+const User = mongoose.model('User', userSchema);
+
+const user = new User({
+  username: 'Pip',
+  password: '12345',
+});
+
+user
+  .save()
+  .then((u) => {
+    console.log('A new user has been added to the database: ', u);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // Init basic route
 app.get('/', (req, res) => {
